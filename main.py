1845f5787a2baba4b98889ba4b61ce6cbbef4bdf
YuuinIH <37664092+YuuinIH@users.noreply.github.com>
2024-02-12 21:05:00 +0800

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ from pydantic import BaseModel
 from typing import Optional
 from fastapi.staticfiles import StaticFiles
 from picamera2 import Picamera2,Preview
-from picamera2.previews.qt import QGlPicamera2
+from picamera2.previews.qt import QPicamera2
 from picamera2.encoders import H264Encoder,MJPEGEncoder
 from picamera2.outputs import FfmpegOutput,FileOutput
 from libcamera import Transform
@@ -96,7 +96,7 @@ async def lifespan(app: FastAPI):
             }
         )
         camera.configure(camera_config)
-        qpicamera2 = QGlPicamera2(camera)
+        qpicamera2 = QPicamera2(camera)
         qpicamera2.showFullScreen()
         camera.start()
         qpicamera2.show()
