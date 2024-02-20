3d872f0715ef3a6658e8c0207920f4011b142e59
YuuinIH <37664092+YuuinIH@users.noreply.github.com>
2024-02-21 01:07:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from app.config import Config
 from app.camera import Camera
 from PySide6.QtWidgets import QApplication
 from sys import argv
+import signal
 
 if __name__ == "__main__":
     app = QApplication(argv)
@@ -10,6 +11,9 @@ if __name__ == "__main__":
     camera = Camera(config)
     preview = PreviewWidget(camera)
     main = MainWindow(config, camera, preview)
+
+    # Handle Ctrl+C
+    signal.signal(signal.SIGINT, signal.SIG_DFL)
 
     main.show()
     exit(app.exec())
